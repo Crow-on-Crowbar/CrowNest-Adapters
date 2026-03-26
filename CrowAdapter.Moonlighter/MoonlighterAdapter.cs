45635d78c38a3a09506f69087383eb19999db62d
mirahavel0309 <parkdev640@gmail.com>
2026-03-27 01:21:43 +0900

    WIP: M7

---
+++ CrowAdapter.Moonlighter/MoonlighterAdapter.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace CrowAdapter.Moonlighter
+{
+    [BepInPlugin("com.crownest.moonlighter", "CrowAdapter.Moonlighter", "1.0.0")]
+    public class MoonlighterAdapter : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("CrowAdapter.Moonlighter loaded!");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.F1))
+            {
+
+            }
+        }
+    }
+}
