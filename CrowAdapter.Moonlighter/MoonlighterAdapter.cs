67e4d57b6f3753391041a46de3bfbcb7a8da8fac
mirahavel0309 <parkdev640@gmail.com>
2026-03-27 12:33:10 +0900

    feat: M7 implement CrowAdapter.Moonlighter base plugin
    - Setup BepInEx C# adapter project
    - Implement F1 hotkey to set gold to 99999
    - Verify game data access via HeroMerchant.Instance

--- CrowAdapter.Moonlighter/MoonlighterAdapter.cs
+++ CrowAdapter.Moonlighter/MoonlighterAdapter.cs
@@ -6,6 +6,7 @@ namespace CrowAdapter.Moonlighter
     [BepInPlugin("com.crownest.moonlighter", "CrowAdapter.Moonlighter", "1.0.0")]
     public class MoonlighterAdapter : BaseUnityPlugin
     {
+
         private void Awake()
         {
             Logger.LogInfo("CrowAdapter.Moonlighter loaded!");
@@ -13,9 +14,12 @@ namespace CrowAdapter.Moonlighter
 
         private void Update()
         {
-            if (Input.GetKeyUp(KeyCode.F1))
+            if (Input.GetKeyDown(KeyCode.F1))
             {
-
+                if (HeroMerchant.Instance == null) return;
+                HeroMerchant.Instance.addGold(-HeroMerchant.Instance.GetCurrentGold());
+                HeroMerchant.Instance.addGold(99999);
+                Logger.LogInfo("Added 99999 gold!");
             }
         }
     }
